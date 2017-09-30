1da17618b353a180fc11ea7a355a2f07d5dea581
nsobczak <nicolas.sobczak@orange.fr>
2017-09-30 16:20:28 +0200

    - add shoots

--- Shmup-poc/Assets/Scripts/PlayerController.cs
+++ Shmup-poc/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,7 @@ public class PlayerController : MonoBehaviour
     public float Speed = 2f;
     public GameObject PrefabShoot;
 
-    SpriteRenderer m_renderer;
     private float m_shootTimer = 0.1f;
-
-    void awake()
-    {
-        m_renderer = GetComponent<SpriteRenderer>();
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
@@ -40,31 +29,35 @@ public class PlayerController : MonoBehaviour
             transform.position += new Vector3(0, -3, 0) * Time.deltaTime * Speed;
         }
 
-//        m_shootTimer -= Time.deltaTime;
-//        if (Input.GetKeyDown(KeyCode.Space))
-//        {
-//            if (m_shootTimer <= 0)
-//            {
-//                float angle = Random.Range(-0.2f, 0.2f) + Mathf.PI / 2.0f;
-//
+        m_shootTimer -= Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (m_shootTimer <= 0)
+            {
+                float angle = Random.Range(-0.2f, 0.2f) + Mathf.PI / 2.0f;
+                GameObject shoot;
+
 //                GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
 //                shoot.GetComponent<ShootComponent>().Direction =
 //                    new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
 //                shoot.GetComponent<ShootComponent>().Speed = Random.Range(4, 6);
-//
-//                //GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
-//                //shoot.GetComponent<ShootComponent>().Direction = new Vector3(-1, 1).normalized;
-//
-//                //GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
-//                //shoot.GetComponent<ShootComponent>().Direction = new Vector3(1, 1).normalized;
-//
-//                //GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
-//                //GameObject.Instantiate(PrefabShoot, transform.position - new Vector3(0.2f, 0, 0), Quaternion.identity);
-//                m_shootTimer = 0.1f; //pour tirer toutes les 100ms
-//            }
-//        }
+
+                shoot = GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
+                shoot.GetComponent<ShootComponent>().Direction = new Vector3(-1, 1).normalized;
+
+                shoot = GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
+                shoot.GetComponent<ShootComponent>().Direction = new Vector3(1, 1).normalized;
+
+                shoot = GameObject.Instantiate(PrefabShoot, transform.position + new Vector3(0.2f, 0, 0), Quaternion.identity);
+                shoot.GetComponent<ShootComponent>().Direction = new Vector3(0, 1);
+                shoot = GameObject.Instantiate(PrefabShoot, transform.position - new Vector3(0.2f, 0, 0), Quaternion.identity);
+                shoot.GetComponent<ShootComponent>().Direction = new Vector3(0, 1);
+
+                m_shootTimer = 0.1f; //pour tirer toutes les 100ms
+            }
+        }
     }
-    
+
     void Move(Vector3 direction)
     {
         transform.position += direction * Time.deltaTime * Speed;
---
+++ Shmup-poc/Assets/Scripts/ShootComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootComponent : MonoBehaviour
+{
+    public float Speed = 10f;
+    public Vector3 Direction;
+
+    void Update()
+    {
+        transform.position += Direction * Time.deltaTime * Speed;
+        GameObject.Destroy(gameObject, 0.5f);
+    }
+
+//    void OnTriggerEnter2D(Collider2D collider)
+//    {
+//        Debug.Log("coucou");
+//        if (collider.gameObject.GetComponent<ShootComponent>())
+//            return;
+//
+//        GameObject.Destroy(gameObject);
+//    }
+}
