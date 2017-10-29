8fcb582558a24f4fa66a4bf0035187b10f9f8bb0
nsobczak <nicolas.sobczak@orange.fr>
2017-10-29 18:39:59 +0100

    - add android quit button support

--- Xeno-Space/Assets/Game Jam Template/Scripts/QuitApplication.cs
+++ Xeno-Space/Assets/Game Jam Template/Scripts/QuitApplication.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuitApplication : MonoBehaviour {
+public class QuitApplication : MonoBehaviour
+{
+    public void Quit()
+    {
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE || UNITY_WEBPLAYER 
+Application.Quit();
 
-	public void Quit()
-	{
-		//If we are running in a standalone build of the game
-	#if UNITY_STANDALONE
-		//Quit the application
-		Application.Quit();
-	#endif
+#elif UNITY_ANDROID
+        AndroidJavaObject activity =
+            new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        activity.Call<bool>("moveTaskToBack", true);
 
-		//If we are running in the editor
-	#if UNITY_EDITOR
-		//Stop playing the scene
-		UnityEditor.EditorApplication.isPlaying = false;
-	#endif
-	}
+        //If we are running in the editor
+#elif UNITY_EDITOR //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 }
